b79f5e8927ec89e3b5b07513d738e8513a40bf62
Vasahlin <Vasahlin@gmail.com>
2024-09-30 22:03:19 +0200

    asd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import userInteraction.UserInteraction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UserInteraction ui = new UserInteraction();
+        ui.start();
+    }
+}
